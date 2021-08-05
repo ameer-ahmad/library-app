e4973f2c33e38db916ae13a353dae9aed60113e1
Ameer Ahmad <ameer5ahmad@gmail.com>
2021-08-05 17:06:30 -0400

    submit button adds new book

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 let myLibrary = [];
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const bookPages = document.getElementById('pages');
+const bookRead = document.getElementById('read');
+const form = document.querySelector('form');
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function Book() {
+    this.title = bookTitle.value;
+    this.author = bookAuthor.value;
+    this.pages = bookPages.value;
+    this.read = bookRead.checked;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(e) {
+    e.preventDefault();
+    if (form.checkValidity()) {
+        myLibrary.push(new Book);
+        bookTitle.value = "";
+        bookAuthor.value = "";
+        bookPages.value = "";
+        bookRead.checked =  false;
+    } else {
+        alert("Please enter all required fields!");
+    }
+
 
 }
+
+// event listeners
+document.getElementById('submit').addEventListener('click', addBookToLibrary);
